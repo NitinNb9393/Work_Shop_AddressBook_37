395ae72fb3a820ba43056f851c7aff09915826a4
Nitin babar <67825116+NitinNb9393@users.noreply.github.com>
2021-11-30 15:20:42 +0530

    Added:

--- src/main/java/com/bridgelabz/Work_Shop_AddressBook_37/AddressBook.java
+++ src/main/java/com/bridgelabz/Work_Shop_AddressBook_37/AddressBook.java
@@ -1,10 +1,11 @@
 package com.bridgelabz.Work_Shop_AddressBook_37;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class AddressBook {
 	static Scanner scanner = new Scanner(System.in);
@@ -156,6 +157,13 @@ public class AddressBook {
 			countofPerson++;
 			System.out.println("total no.of.persons: " + countofPerson);
 		}
+	}
+
+	public void sortByName() {
+		addressbook.keySet().forEach((String name) -> {
+			addressbook.get(name).stream().sorted(Comparator.comparing(Contact::getFirstname))
+					.collect(Collectors.toList()).forEach(person -> System.out.println(person.toString()));
+		});
 	}
 
 	public void displayList() {
