9a8c09699e5e34e5f56c5d338299a38a978bdfca
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-30 14:05:54 +0530

    Added : Program for the Create Contact in Address book system

---
+++ src/main/java/com/bridgelabz/Work_Shop_AddressBook_37/AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Work_Shop_AddressBook_37;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System");
+		Contacts contact = new Contacts("Nitin", "Babar", "karad", "satara", "Maharashtra", 410553, "8237259393",
+				"nitinbabar9393@gmail.com");
+		System.out.println(contact);
+	}
+
+}
